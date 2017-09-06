81aef63dd47acfc8259fafab48eb855f85c65d7d
TgoislardSIO <thomas.goislard.sio@gmail.com>
2017-09-06 15:44:07 +0200

    getheure,getminute

--- Heure.java
+++ Heure.java
@@ -3,9 +3,18 @@ public class Heure {
 	private int heure ;
 	private int minute ;
 	
-	public class Heure (heure;minute){
+	public class Heure (int heure,int minute){
 		heure = this.heure;
 		minute = this.minute;
-		}
+	}
+		
+	public class getHeure () {
+		return heure ;
+		
+	}
 	
-	} ;
+	public class getminute() {
+		return minute ;
+		
+	}
+}
